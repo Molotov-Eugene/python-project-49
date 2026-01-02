1000326d51429c37ceaf7e57e6c740d1cb600e9d
Eugene Molotov <forever_air@Eugenes-MacBook-Air.local>
2026-01-02 10:23:53 +0500

    step 4 complete

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 from brain_games.scripts.welcome_user import welcome_user
 
+
 def main():
     name = welcome_user()
-    print (f'Hello, {name}!')
+    print(f'Hello, {name}!')
+
 
 if __name__ == "__main__":
     main()
